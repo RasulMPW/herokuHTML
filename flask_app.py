ab9d2845c591a74e79357800053081731dd10dba
RasulMPW <80685966+RasulMPW@users.noreply.github.com>
2021-03-22 11:28:41 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, W orl d!'
+
+    return render_template('welcome.html')
+@app.route('/first')
+def first():
+    return render_template('first.html')
+@app.route('/home')#
+def login():
+    return render_template('login.html')
+
